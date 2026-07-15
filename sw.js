e1d61710ac73f798f6e63195f74fc30a409a831a
viralking200220-source <viralking200220@gmail.com>
2026-07-15 15:34:40 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+﻿const cacheName = 'outils-v1';
+const assets = ['index.html', 'manifest.json', 'icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
